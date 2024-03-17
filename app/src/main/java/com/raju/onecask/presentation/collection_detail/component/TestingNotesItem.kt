aaeac356930f279f942e55c76bf9f9559c096377
Raju Chettri <dada@Rajus-MacBook-Pro.local>
2024-03-17 16:00:55 -0400

    tasting notes screen done

---
+++ app/src/main/java/com/raju/onecask/presentation/collection_detail/component/TestingNotesItem.kt
@@ -0,0 +1,55 @@
+package com.raju.onecask.presentation.collection_detail.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.raju.onecask.R
+import com.raju.onecask.ui.theme.COLOR_111c20
+import com.raju.onecask.ui.theme.COLOR_E7E9EA
+
+@Composable
+fun TestingNotesItem(
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = COLOR_111c20,
+        ),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(8.dp),
+        ) {
+            Text(
+                text = "Nose",
+                fontFamily = FontFamily(Font(R.font.eb_garamond)),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.W500,
+                color = COLOR_E7E9EA,
+                lineHeight = TextUnit(28F, TextUnitType.Sp),
+            )
+            Text(
+                text = "Description",
+                fontFamily = FontFamily(Font(R.font.lato)),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.W400,
+                color = COLOR_E7E9EA,
+                letterSpacing = TextUnit(0.5F, TextUnitType.Sp),
+                lineHeight = TextUnit(24F, TextUnitType.Sp),
+            )
+        }
+
+    }
+
+}
